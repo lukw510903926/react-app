1f4f331274af1ddfba8e21e3622ee7743844984f
lkw510903926 <13507615840@.163>
2018-06-28 16:26:01 +0800

    表格

--- src/components/LeftMenu.js
+++ src/components/LeftMenu.js
@@ -2,9 +2,10 @@ import { Menu, Icon } from "antd";
 import React from "react";
 
 const SubMenu = Menu.SubMenu;
+const Item = Menu.Item
 
 class LeftMenu extends React.Component {
-  // submenu keys of first level
+
   rootSubmenuKeys = ["sub1", "sub2", "sub4"];
   state = {
     openKeys: ["sub1"]
@@ -23,25 +24,19 @@ class LeftMenu extends React.Component {
   render() {
     return (
       <Menu mode="inline" theme="dark" openKeys={this.state.openKeys} onOpenChange={this.onOpenChange}>
-        <SubMenu key="sub1" title={<span><Icon type="mail"/><span>Navigation One</span></span>}>
-          <Menu.Item key="1">Option 1</Menu.Item>
-          <Menu.Item key="2">Option 2</Menu.Item>
-          <Menu.Item key="3">Option 3</Menu.Item>
-          <Menu.Item key="4">Option 4</Menu.Item>
-        </SubMenu>
-        <SubMenu key="sub2" title={<span><Icon type="appstore"/><span>Navigation Two</span></span>}>
-          <Menu.Item key="5">Option 5</Menu.Item>
-          <Menu.Item key="6">Option 6</Menu.Item>
+        <SubMenu key="sub1" title={<span><Icon type="mail"/><span>组件</span></span>}>
+          <Item key="1">表格</Item>
+          <Item key="2">表单</Item>
           <SubMenu key="sub3" title="Submenu">
-            <Menu.Item key="7">Option 7</Menu.Item>
-            <Menu.Item key="8">Option 8</Menu.Item>
+            <Item key="7">Option 7</Item>
+            <Item key="8">Option 8</Item>
           </SubMenu>
         </SubMenu>
         <SubMenu key="sub4" title={<span><Icon type="setting"/><span>Navigation Three</span></span>}>
-          <Menu.Item key="9">Option 9</Menu.Item>
-          <Menu.Item key="10">Option 10</Menu.Item>
-          <Menu.Item key="11">Option 11</Menu.Item>
-          <Menu.Item key="12">Option 12</Menu.Item>
+          <Item key="9">Option 9</Item>
+          <Item key="10">Option 10</Item>
+          <Item key="11">Option 11</Item>
+          <Item key="12">Option 12</Item>
         </SubMenu>
       </Menu>
     );
---
+++ src/components/Table.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default class Table extends React.Component {
+
+    render(){
+        return <div>
+            表格
+        </div>
+    }
+}
