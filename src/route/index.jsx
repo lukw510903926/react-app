4b324ff4178c7c1a4a3f7b629d0a38fac0b13c40
lukw510903926 <13507615840@163.com>
2018-07-27 22:41:55 +0800

    cookie 存储

--- src/route/index.jsx
+++ src/route/index.jsx
@@ -1,54 +1,53 @@
 import React from "react";
-import {Route, Redirect, Switch, HashRouter as Router, withRouter} from "react-router-dom";
+import { Route, Redirect, Switch, HashRouter as Router, withRouter } from "react-router-dom";
 import routes from "./route";
-import Constants from '../util/Constants'
+import Constants from "../util/Constants";
 import LocalStore from "../util/LocalStore";
 
 class ReactRoute extends React.Component {
 
-    constructor(props) {
-        super(props)
-        let routeMap = {}
-        routes.forEach(route => routeMap[route.key] = route)
-        this.state = {
-            routeMap: routeMap
-        }
-    }
+  constructor(props) {
+    super(props);
+    let routeMap = {};
+    routes.forEach(route => routeMap[route.key] = route);
+    this.state = {
+      routeMap: routeMap
+    };
+  }
 
-    /**
-     * 判断是否登录
-     * @param route 路由信息
-     */
-    componentDidUpdate() {
-        let currentPath = this.props.location.pathname
-        let route = this.state.routeMap[currentPath]
-        document.title = route ? route.title : 'react'
-    }
+  /**
+   * 判断是否登录
+   * @param route 路由信息
+   */
+  componentDidUpdate(){
+    let currentPath = this.props.location.pathname;
+    let route = this.state.routeMap[currentPath];
+    document.title = route ? route.title : "react";
+  }
 
-    render() {
-        let loginUser = LocalStore.getItem(Constants.LOGIN_USER)
-        let hasLogin = loginUser ? true : false
-        return <Router>
-            <Switch>
-                <Route exact path="/home" render={() => <Redirect to="/home/form"/>}/>
-                {
-                    // routes.map(route => <Route {...route} exact component={ () => !route.auth ? <route.name /> : <Redirect to={'/login'}/> }/>)
-                    routes.map(route => <Route {...route} component={
-                        props => (route.auth && !hasLogin ?
-                            (<Redirect {...props} to={{pathname: '/login', state: {from: props.location}}}/>)
-                            : (<route.name {...props}/>))
-                    }/>)
-                }
-                {
-                    /*  <Route path="/login" component={Login}/>
-                  <Route exact path="/form" component={ReactForm}/>
-                  <Route exact path="/table" component={Grid}/>
-                  <Route exact path="/product" component={Product}/>
-                  <Route exact path="/product/list" component={ProductList}/>*/}
-                <Redirect from='*' to='/home/404'/>
-            </Switch>
-        </Router>
-    }
+  getRoutes(hasLogin) {
+
+    return routes.map(route => <Route {...route} component={
+      props => (route.auth && !hasLogin ?
+        (<Redirect {...props} to={{ pathname: "/login", state: { from: props.location } }}/>) :
+        (<route.name {...props}/>))
+    }/>);
+    // routes.map(route => <Route {...route} exact component={ () => !route.auth ? <route.name /> : <Redirect to={'/login'}/> }/>)
+  }
+
+  render() {
+    let loginUser = LocalStore.getItem(Constants.LOGIN_USER);
+    let hasLogin = loginUser ? true : false;
+    return (
+      <Router>
+        <Switch>
+          <Route exact path="/home" render={() => <Redirect to="/home/form"/>}/>
+          {this.getRoutes(hasLogin)}
+          <Redirect from='*' to='/home/404'/>
+        </Switch>
+      </Router>
+    );
+  }
 }
 
-export default withRouter(ReactRoute)
+export default withRouter(ReactRoute);
--- src/util/LocalStore.js
+++ src/util/LocalStore.js
@@ -1,10 +1,47 @@
+import { Cookies } from "react-cookie";
+
 export default class LocalStore {
 
-    static setItem(key, value) {
-        sessionStorage.setItem(key, value)
-    }
+  static cookies = new Cookies();
 
-    static getItem(key) {
-        return sessionStorage.getItem(key)
+  static default_time_out = 30 * 60 * 1000;
+
+  static setItem(key, value) {
+    localStorage.setItem(key, value);
+  }
+
+  static getItem(key) {
+    return localStorage.getItem(key);
+  }
+
+  static clearStorage() {
+    localStorage.clear();
+  }
+
+  static setSessionItem(key, value) {
+    sessionStorage.setItem(key, value);
+  }
+
+  static getSessionItem(key) {
+    return sessionStorage.getItem(key);
+  }
+
+  static clearSessionItem() {
+    sessionStorage.clear();
+  }
+
+  static setCookie(key, value, options = {}) {
+
+    if (!options.expires) {
+      let now = new Date().getTime();
+      options.expires = new Date(now + this.default_time_out);
     }
+    this.cookies.set(key, value, options);
+  }
+
+  static deleteCookie(key, options = {}) {
+    let now = new Date().getTime();
+    options.expires = new Date(now - this.default_time_out);
+    this.cookies.remove(key, options);
+  }
 }
