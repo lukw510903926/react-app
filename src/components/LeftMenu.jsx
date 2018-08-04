94f8a3128f32bf572aa7420bd9134ffd7a5a534d
lukw510903926 <13507615840@163.com>
2018-08-04 11:15:06 +0800

    菜单调整

--- src/components/LeftMenu.jsx
+++ src/components/LeftMenu.jsx
@@ -1,59 +1,56 @@
-import {Menu, Icon} from "antd";
+import { Menu, Icon } from "antd";
 import React from "react";
 import routes from "../route/route";
-import {Link, withRouter} from "react-router-dom";
+import { Link, withRouter } from "react-router-dom";
 
 class LeftMenu extends React.Component {
 
-    constructor(props) {
-        super(props);
-        let rootSubmenuKeys = routes.map(entity => entity.key);
-        let openKeys = [rootSubmenuKeys[0]];
-        this.state = {
-            openKeys,
-            rootSubmenuKeys
-        };
-    }
-
-    onOpenChange = (openKey) => {
-        const latestOpenKey = openKey.find(key => this.state.openKeys.indexOf(key) === -1);
-        if (this.state.rootSubmenuKeys.includes(latestOpenKey)) {
-            this.setState({openKeys: openKey});
-        } else {
-            this.setState({
-                openKeys: latestOpenKey ? [latestOpenKey] : []
-            });
-        }
+  constructor(props) {
+    super(props);
+    let submenuKeys = routes.map(entity => entity.key);
+    let openKeys = [submenuKeys[0]];
+    this.state = {
+      openKeys,
+      submenuKeys
     };
+  }
 
-    getMenuItem() {
-
-        let menu = [];
-        routes.forEach(entity => {
-            if (entity.children) {
-                menu.push(<Menu.SubMenu key={entity.key} title={<span><Icon type={entity.type}/>{entity.title}</span>}>
-                    {
-                        entity.children.map(item => {
-                            if (item.isMenu) {
-                                return <Menu.Item key={item.key}><Link to={item.path}>{item.title}</Link></Menu.Item>;
-                            }
-                        })
-                    }
-                </Menu.SubMenu>);
-            } else if (entity.isMenu) {
-                return <Menu.Item key={entity.key}><Link to={entity.path}>{entity.title}</Link></Menu.Item>;
-            }
-        });
-        return menu;
+  onOpenChange = (openKey) => {
+    const latestOpenKey = openKey.find(key => this.state.openKeys.indexOf(key) === -1);
+    if (this.state.submenuKeys.includes(latestOpenKey)) {
+      this.setState({ openKeys: openKey });
+    } else {
+      this.setState({
+        openKeys: latestOpenKey ? [latestOpenKey] : []
+      });
     }
+  };
 
-    render() {
-        return (
-            <Menu mode="inline" theme="dark" selectedKeys={[this.props.location.pathname]} openKeys={this.state.openKeys} onOpenChange={this.onOpenChange}>
-                {this.getMenuItem()}
-            </Menu>
-        );
-    }
+  getMenuItem() {
+
+    let menu = [];
+    routes.forEach(entity => {
+      if (entity.children) {
+        menu.push(<Menu.SubMenu key={entity.key} title={<span><Icon type={entity.type}/>{entity.title}</span>}>
+          {
+            entity.children.map(item => item.isMenu ? <Menu.Item key={item.key}><Link to={item.path}>{item.title}</Link></Menu.Item> : null)
+          }
+        </Menu.SubMenu>);
+      } else {
+        menu.push(<Menu.Item key={entity.key}><Link to={entity.path}>{entity.title}</Link></Menu.Item>)
+      }
+    });
+    return menu;
+  }
+
+  render() {
+    let selectedKeys = this.props.location.pathname;
+    return (
+      <Menu mode="inline" theme="dark" selectedKeys={[selectedKeys]} openKeys={this.state.openKeys} onOpenChange={this.onOpenChange}>
+        {this.getMenuItem()}
+      </Menu>
+    );
+  }
 }
 
 export default withRouter(LeftMenu);
