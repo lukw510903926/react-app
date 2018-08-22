a9e86cd9e41dbf9e914861862fb59c5152201fcd
lukw510903926 <13507615840@163.com>
2018-08-22 22:36:34 +0800

    菜单调整

--- src/util/http/FetchHttp.js
+++ src/util/http/FetchHttp.js
@@ -1,40 +1,40 @@
 export default class FetchHttp {
 
-    static getHeader() {
-        var headers = new Headers();
-        headers.append('Content-Type', 'text/plain');
-        headers.append('X-My-Custom-Header', 'CustomValue');
-        headers.append('CLOUD_HEADER', 'CLOUD_HEADER_VALUE');
-        headers.set('Content-Type', 'application/json');
-        return headers
-    }
+  static getHeader() {
+    let headers = new Headers();
+    headers.append("Content-Type", "text/plain");
+    headers.append("X-My-Custom-Header", "CustomValue");
+    headers.append("CLOUD_HEADER", "CLOUD_HEADER_VALUE");
+    headers.set("Content-Type", "application/json");
+    return headers;
+  }
 
-    static getRequest = (url, callback) => {
-        fetch(url, {
-            method: 'GET',
-            credentials: 'include',
-            headers: FetchHttp.getHeader()
-        }).then(response => response.json().then(data => callback(data))
-        ).catch(err => console.log("Fetch错误:" + err))
-    }
+  static getRequest = (url, callback) => {
+    fetch(url, {
+      method: "GET",
+      credentials: "include",
+      headers: FetchHttp.getHeader()
+    }).then(response => response.json().then(data => callback(data))
+    ).catch(err => console.log("Fetch错误:" + err));
+  };
 
-    static postRequest = (url, params, callback) => {
-        var data = new FormData();
-         data.append("name","hello");
-        fetch(url, {
-            method: 'post',
-            body: JSON.stringify(params),
-            mode:'cors',
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json',
-            }
-        }).then((response) => {
-            response.json().then(function (data) {
-                callback(data)
-            })
-        }).catch(function (error) {
-            console.info(error)
-        })
-    }
+  static postRequest = (url, params, callback) => {
+    let data = new FormData();
+    data.append("name", "hello");
+    fetch(url, {
+      method: "post",
+      body: JSON.stringify(params),
+      mode: "cors",
+      headers: {
+        "Accept": "application/json",
+        "Content-Type": "application/json"
+      }
+    }).then((response) => {
+      response.json().then(function(data) {
+        callback(data);
+      });
+    }).catch(function(error) {
+      console.info(error);
+    });
+  };
 }
