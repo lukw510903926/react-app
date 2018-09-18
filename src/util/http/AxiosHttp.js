e5df95c9f507446d05481cda0f10549e8e12c624
lkw510903926 <13507615840@163.com>
2018-09-18 11:33:54 +0800

    配置修改

--- src/util/http/AxiosHttp.js
+++ src/util/http/AxiosHttp.js
@@ -4,77 +4,77 @@ axios.defaults.baseURL = "/api";
 axios.defaults.timeout = 6000;
 axios.defaults.headers["CLOUD_HEADER"] = "application/x-www-form-urlencoded";
 axios.interceptors.request.use(
-  (config) => {
-    console.info(config);
-    return config;
-  }, error => {
-    return Promise.reject(error);
-  }
+    (config) => {
+        console.info(config);
+        return config;
+    }, error => {
+        return Promise.reject(error);
+    }
 );
 
 axios.interceptors.response.use(
-  (response) => {
-    console.info("response interceptors-------------");
-    return response;
-  }, (error) => {
-    return Promise.reject(error);
-  }
+    (response) => {
+        console.info("response interceptors-------------");
+        return response;
+    }, (error) => {
+        return Promise.reject(error);
+    }
 );
 
 export default class AxiosHttp {
 
-  static getRequest = (url) => {
-    return new Promise((resolve, reject) => {
-      axios.get(url).then(
-        response => resolve(response)
-      ).catch(error => reject(error));
-    });
-  };
-
-  static postRequest(url, params) {
-    return new Promise((resolve, reject) => {
-      axios.post(url, params).then(
-        response => resolve(response)
-      ).catch(
-        error => reject(error));
-    });
-  }
-
-
-  static deleteRequest(url, params) {
-    return new Promise((resolve, reject) => {
-      axios.delete(url, params).then(
-        response => resolve(response)
-      ).catch(
-        error => reject(error));
-    });
-  }
-
-  static putRequest(url, params) {
-    return new Promise((resolve, reject) => {
-      axios.put(url, params).then(
-        response => resolve(response)
-      ).catch(
-        error => reject(error));
-    });
-  }
-
-
-  static exportRequest(url, params) {
-    let config = {
-      responseType: "arraybuffer"
+    static getRequest = (url) => {
+        return new Promise((resolve, reject) => {
+            axios.get(url).then(
+                response => resolve(response.data)
+            ).catch(error => reject(error));
+        });
     };
-    return new Promise((resolve, reject) => {
-      axios.post(url, params, config).then(
-        response => resolve(response)
-      ).catch(
-        error => reject(error));
-    });
-  }
 
-  static handleException(response) {
-    if (console) {
-      console.info(response);
+    static postRequest(url, params) {
+        return new Promise((resolve, reject) => {
+            axios.post(url, params).then(
+                response => resolve(response.data)
+            ).catch(
+                error => reject(error));
+        });
     }
-  }
+
+
+    static deleteRequest(url, params) {
+        return new Promise((resolve, reject) => {
+            axios.delete(url, params).then(
+                response => resolve(response.data)
+            ).catch(
+                error => reject(error));
+        });
+    }
+
+    static putRequest(url, params) {
+        return new Promise((resolve, reject) => {
+            axios.put(url, params).then(
+                response => resolve(response.data)
+            ).catch(
+                error => reject(error));
+        });
+    }
+
+
+    static exportRequest(url, params) {
+        let config = {
+            responseType: "arraybuffer"
+        };
+        return new Promise((resolve, reject) => {
+            axios.post(url, params, config).then(
+                response => resolve(response.data)
+            ).catch(
+                error => reject(error));
+        });
+    }
+
+    static handleException(response) {
+        if (console) {
+            console.info(response);
+        }
+    }
 }
